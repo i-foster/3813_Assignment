49fc29fce6b45bd589a85f35e43b8a22ba48f5ee
i-foster <nakor27@live.com>
2024-08-31 13:50:49 +1000

    deleted uneeded line

--- ChatApp/chatApp/src/app/app.component.ts
+++ ChatApp/chatApp/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
