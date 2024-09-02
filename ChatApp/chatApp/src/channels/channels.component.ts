93d9028cf8947521b465b40118d612228d6b135a
i-foster <nakor27@live.com>
2024-09-02 11:24:33 +1000

    Channel print out from local storage

--- ChatApp/chatApp/src/channels/channels.component.ts
+++ ChatApp/chatApp/src/channels/channels.component.ts
@@ -15,15 +15,51 @@ export class ChannelsComponent {
 
   }
 
+
   ngOnInit():void {
     this.Currentgroup()}
 
- Currentgroup(){   if (localStorage.getItem("group") == null){
-      this.router.navigateByUrl("home")
+  channels =[""];
+  /*group1 = ["welcome", "introduce yourself", "current Happenings"];
+  group2 = ["admin", "Complaints", "Suggestions"];
+  group3 = ["Movies", "TV", "Books", "Music"];
+  group4 = ["Music", "welcome"];
+  groups = []
+  groupNames = ["group1","group2"]*/
+  
+
+ Currentgroup(){
+  type groupsModel = {
+    groupName:string;
+    groupChannels:string[];
+  };
+  
+  let groups:groupsModel[] = [
+    {groupName:"group1", groupChannels:["welcome", "introduce yourself", "current Happenings"]},
+    {groupName:"group2", groupChannels:["admin", "Complaints", "Suggestions"]},
+  ]
+  
+  if(localStorage.getItem("group") == null){
+      this.router.navigateByUrl("home");
+  }else {
+
+    let selectedGroup:any = localStorage.getItem("group");
+    let cleanString: string = selectedGroup.trim().replace(/\s+/g, "");
+    selectedGroup = cleanString;    
+    for (let i = 0; i < groups.length; i++){
+
+      console.log(groups[i])
+      if(selectedGroup == groups[i].groupName){
+      this.channels = groups[i].groupChannels
+  }}
+}
+
   }
+  channelselect(a:any){
+
   }
   home(){
-    this.router.navigateByUrl("home");
+    //this.router.navigateByUrl("home");
   }
   profile(){
     this.router.navigateByUrl("profile")
