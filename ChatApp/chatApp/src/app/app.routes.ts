b3cb04b8bf37364f43e2b7dd9633def0a5f80672
i-foster <nakor27@live.com>
2024-09-04 10:00:28 +1000

    css for login and home page, updated routers to remove unneeded one.

--- ChatApp/chatApp/src/app/app.routes.ts
+++ ChatApp/chatApp/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { ProfileComponent } from '../profile/profile.component';
 import { LoginComponent } from '../login/login.component';
 import { HomeComponent } from '../home/home.component';
-import { SignupComponent } from '../signup/signup.component';
 import { ChannelsComponent } from '../channels/channels.component';
 import { ChatsComponent } from '../chats/chats.component';
 
@@ -10,7 +9,6 @@ export const routes: Routes = [
     {path: 'profile', component:ProfileComponent},
     {path: '', component:LoginComponent},
     {path: 'home', component:HomeComponent},
-    {path: 'signup', component:SignupComponent},
     {path: 'channels', component:ChannelsComponent},
     {path: 'chats', component:ChatsComponent}
 ];
